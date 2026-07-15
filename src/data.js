2c1d5dbebff77d06e7f120b5f81d4e14dd1c158a
JLindstrom-US <lindstrom.a.james@gmail.com>
2026-07-14 19:06:05 -0500

    removed junk

--- src/data.js
+++ src/data.js
@@ -32,7 +32,6 @@ export const raids = [
   { name: 'Last Wish', marks: 2 },
   { name: 'Garden of Salvation', marks: 2 },
   { name: 'Deep Stone Crypt - Normal', marks: 2 },
-  { name: 'Deep Stone Crypt - Master', marks: 3 },
   { name: 'Vault of Glass - Normal', marks: 2 },
   { name: 'Vault of Glass - Master', marks: 3 },
   { name: 'Vow of the Disciple - Normal', marks: 2 },
